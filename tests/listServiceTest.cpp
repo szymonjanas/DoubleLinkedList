0ec82e126ad112e256527005f632204eac686e71
[szymon] <[szymon.janas.95@gmail.com]>
2018-10-03 14:00:24 +0200

    added test push_front and tests of increment list_size

--- tests/listServiceTest.cpp
+++ tests/listServiceTest.cpp
@@ -13,17 +13,19 @@ auto make_ptr(int value)
     return val;
 }
 
-TEST_F( testList, push_back )
+TEST_F( testList, push_back_and_increment_list_size )
 {
     //PUSH BACK TO EMPTY LIST
     ls.push_back(make_ptr(111));
     ASSERT_EQ(ls.getHead()->value, 111);
     ASSERT_EQ(ls.getTail()->value, 111);
+    ASSERT_EQ(ls.getListSize(), 1);
 
     //PUSH BACK TO LIST WITH ONE ELEMENT
     ls.push_back(make_ptr(222));
     ASSERT_EQ(ls.getHead()->value, 111);
     ASSERT_EQ(ls.getTail()->value, 222);
+    ASSERT_EQ(ls.getListSize(), 2);
 
     //PUSH BACK TO LIST WITH ELEMENTS
     ls.push_back(make_ptr(333));
@@ -33,5 +35,33 @@ TEST_F( testList, push_back )
     ls.push_back(make_ptr(555));
     ASSERT_EQ(ls.getHead()->value, 111);
     ASSERT_EQ(ls.getTail()->value, 555);
+
+    ASSERT_EQ(ls.getListSize(), 5);
+
+}
+
+TEST_F( testList, push_front_and_increment_list_size )
+{
+    //PUSH FRONT TO EMPTY LIST
+    ls.push_front(make_ptr(555));
+    ASSERT_EQ(ls.getHead()->value, 555);
+    ASSERT_EQ(ls.getTail()->value, 555);
+    ASSERT_EQ(ls.getListSize(), 1);
+
+    //PUSH FRONT TO LIST WITH ONE ELEMENT
+    ls.push_front(make_ptr(444));
+    ASSERT_EQ(ls.getHead()->value, 444);
+    ASSERT_EQ(ls.getTail()->value, 555);
+    ASSERT_EQ(ls.getListSize(), 2);
+
+    //PUSH FRONT TO LIST WITH ELEMENTS
+    ls.push_front(make_ptr(333));
+    ASSERT_EQ(ls.getHead()->value, 333);
+    ls.push_front(make_ptr(222));
+    ASSERT_EQ(ls.getHead()->value, 222);
+    ls.push_front(make_ptr(111));
+    ASSERT_EQ(ls.getHead()->value, 111);
+    ASSERT_EQ(ls.getTail()->value, 555);
+    ASSERT_EQ(ls.getListSize(), 5);
 
 }
